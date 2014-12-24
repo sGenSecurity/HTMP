f6dcd7e2e516852ec8a98c6cd8c104a8cbda3c1a
Leeyoochan <L@Leeyoochans-MacBook-Pro.local>
2014-12-25 01:20:09 +0900

    add function b64 edit table

--- b64.py
+++ b64.py
@@ -1,12 +1,37 @@
 import base64
+import string
 
-class Base64():
 
-	def encode(text):
-		return base64.b64encode(text)
 
-	def decode(text):
-		return base64.b64decode(text)
 
-	def et_encode(text, table):
-		
+def b64encode(text):
+	
+	return base64.b64encode(text)
+
+def b64decode(text):
+	
+	return base64.b64decode(text)
+
+def b64encode_edit(text, edit_base64chars):
+	
+	if type(edit_base64chars) is not str:
+		print "[-] The type of arguments must be str"
+	
+	std_base64chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+
+	
+
+	return (base64.b64encode(text)).translate(string.maketrans(edit_base64chars,std_base64chars))
+
+def b64decode_edit(text, edit_base64chars):
+	
+	if type(edit_base64chars) is not str:
+		print "[-] The type of arguments must be str"
+
+	std_base64chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+	
+	return base64.b64decode(text.translate(string.maketrans(std_base64chars, edit_base64chars)))
+
+
+
+
