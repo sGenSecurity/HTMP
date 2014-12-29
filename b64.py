15415dfb37a67b57261d70cf6a7e549e5038606e
Leeyoochan <L@Leeyoochans-MacBook-Pro.local>
2014-12-29 11:26:01 +0900

    add condition

--- b64.py
+++ b64.py
@@ -16,10 +16,9 @@ def b64encode_edit(text, edit_base64chars):
 	
 	if type(edit_base64chars) is not str:
 		print "[-] The type of arguments must be str"
-	
+	if len(edit_base64chars) != 64:
+		print "[-] table length must be 64"
 	std_base64chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
-
-	
 
 	return (base64.b64encode(text)).translate(string.maketrans(edit_base64chars,std_base64chars))
 
@@ -27,6 +26,8 @@ def b64decode_edit(text, edit_base64chars):
 	
 	if type(edit_base64chars) is not str:
 		print "[-] The type of arguments must be str"
+	if len(edit_base64chars) != 64:
+		print "[-] table length must be 64"
 
 	std_base64chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
 	
