5ee485dadd09cba24399bae132682f5682bfbe2e
Minyung <resone9@naver.com>
2015-01-09 07:26:48 +0000

    md5 modify

---
+++ user_md5.py
@@ -0,0 +1,28 @@
+import urllib
+import md5
+
+def encrypt(str):
+	return md5.md5(str).hexdigest()
+
+def decrypt(md5):
+	url = 'http://www.md5online.org/md5-decrypt.html'
+
+	#md5 = "4124bc0a9335c27f086f24ba207a4912"
+
+	params = urllib.urlencode({
+		'md5' : md5
+	})
+
+	data = urllib.urlopen(url, params).read()
+
+	fIndex = data.find("Found : ")
+	
+	if fIndex == -1:
+		return -1
+	
+	data = data[(fIndex+11):]
+	fIndex = data.find("</b>")
+	data = data[:fIndex]
+	#	print("Found : ")
+	return data
+
